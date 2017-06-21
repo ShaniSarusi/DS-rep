7972530f76c4b46a3992ab8a0369999a639e2b2f
itzik-mazeh-intel <itzik.mazeh@intel.com>
2017-06-21 07:59:10 +0300

    remove old analysis_main versions + add TSFresh features extraction to the new pipeline

--- Sandbox/Itzik/old_files/analysis_main_temp.py
+++ Sandbox/Itzik/old_files/analysis_main_temp.py
@@ -8,9 +8,8 @@ Created on Thu Jun  8 12:57:44 2017
 import os
 import numpy as np
 from future.utils import lmap
-#import datetime as dt
+import datetime as dt
 import pandas as pd
-from tsfresh import extract_features
 
 '''
 Avishai's settings:
@@ -22,13 +21,12 @@ os.chdir(os.getcwd()+"/DataScientists")
 Itzik's settings:
 '''
 data_path = 'C:\\Users\\imazeh\\Itzik\\Health_prof\\L_Dopa\\Large_data\\'
-os.chdir('C:\\Users\\imazeh/Itzik/Health_prof/git_team/DataScientists/')
+os.chdir('C:\\Users\\imazeh/Itzik/Health_prof/git4/DataScientists')
     
-from Utils.Features import WavTransform
-from Utils.Features import TSFresh
+from Utils.features import WavTransform
 import Utils.Preprocessing.projections as projections
 import Utils.Preprocessing.denoising as Denoiseing_func
-import LDopa.DataReading.ReadTheDataFromLDOPA as data_reading
+import LDopa.Data_reading.ReadTheDataFromLDOPA as data_reading
 import LDopa.Classification.classifier as classifier
 import LDopa.Evaluation.evaluation as evaluation
 
@@ -37,90 +35,54 @@ import LDopa.Evaluation.evaluation as evaluation
 Read with SQL
 """
 res = data_reading.ReadAllData("ConnectSmoove2")
-res = data_reading.ArrangeRes(res,path='C:/Users/awagner')
+res = data_reading.ArrangeRes(res,path = 'C:/Users/awagner')
 tags_df, lab_x, lab_y, lab_z,lab_n = data_reading.MakeIntervalFromAllData(res,25,2,1,1,50)
 
 #######
 """
 Read Itizk
 """
-exec(open('./LDopa/Data_reading/load_from_csv.py').read())
+exec(open('./LDopa/data_reading/load_from_csv.py').read())
 tags_df = read_tag_data(data_path)
 lab_x, lab_y, lab_z = read_data_windows(data_path, read_also_home_data=False, sample_freq=50, window_size=5)
 #####
 
+
 '''
-Read data - new approach:
+Perform transformation on the data:
+    -----------------IF NEEDED, CHANGE THE FUNCTIONS EXECUTING THE PROJECTION--------------------
 '''
-res = pd.read_csv(data_path+'AllLabData.csv')
-res = res.drop('Unnamed: 0', 1)
-res = data_reading.ArrangeRes(res,path = 'LDopa/DataReading/Resources/mapTasksClusters.csv')
-tags_df, lab_x, lab_y, lab_z, lab_n = data_reading.MakeIntervalFromAllData(res,5,2.5,1,1,50)
-lab_x_numpy = lab_x.as_matrix(); lab_x = lab_x_numpy[:,range(len(lab_x_numpy[0])-1)]
-lab_y_numpy = lab_y.as_matrix(); lab_y = lab_y_numpy[:,range(len(lab_x_numpy[0])-1)]
-lab_z_numpy = lab_z.as_matrix(); lab_z = lab_z_numpy[:,range(len(lab_x_numpy[0])-1)]
+#Project data from 3 to 2 dimensions:
+lab_ver_proj, lab_hor_proj = projections.project_from_3_to_2_dims(lab_x, lab_y, lab_z)
+
+'''
+Perform signal denoising:
+    -----------------IF NEEDED, CHANGE THE FUNCTIONS EXECUTING THE DENOISING--------------------
+'''
+lab_ver_denoised = Denoiseing_func.denoise_signal(lab_ver_proj)
+lab_hor_denoised = Denoiseing_func.denoise_signal(lab_hor_proj)
 
 
 '''
-Build an indicator vector, which will indicate which records are relevant for
-the analysis, with regards to the specific task and symptom:
+Extract features:
 '''
+#Create features for each projected dimension, and stack both dimensions horizontally:
+WavFeatures = WavTransform.wavtransform()
+lab_ver_features = WavFeatures.createWavFeatures(lab_ver_denoised)
+lab_hor_features = WavFeatures.createWavFeatures(lab_hor_denoised)
+features_data = np.column_stack((lab_ver_features, lab_hor_features))
+
+'''
+Prepare the data for the classification process:
+'''
+#Build an indicator vector, which will indicate which records are relevant for the analysis:
 task_names = tags_df.Task.as_matrix()
 task_clusters = tags_df.TaskClusterId.as_matrix()
 relevant_task_names = []
 relevant_task_clusters = [5] # 1=resting, 4=periodic hand movement, 5=walking
 cond = np.asarray(lmap(lambda x: x in relevant_task_clusters, task_clusters))
 
-
-'''
-Perform transformation on the data:
-'''
-#Project data from 3 to 2 dimensions:
-lab_ver_proj, lab_hor_proj = projections.project_from_3_to_2_dims(lab_x, lab_y,
-                                                                  lab_z)
-
-
-'''
-Filter the data according to the condition vector:
-'''
-sub_lab_ver_proj = lab_ver_proj[cond==True]
-sub_lab_hor_proj = lab_hor_proj[cond==True]
-
-
-'''
-Perform signal denoising:
-'''
-lab_ver_denoised = Denoiseing_func.denoise_signal(sub_lab_ver_proj)
-lab_hor_denoised = Denoiseing_func.denoise_signal(sub_lab_hor_proj)
-
-
-'''
-Extract features:
-'''
-#Create wavelet features for each projected dimension,
-#and stack both dimensions horizontally:
-WavFeatures = WavTransform.wavtransform()
-lab_ver_features = WavFeatures.createWavFeatures(lab_ver_denoised)
-lab_hor_features = WavFeatures.createWavFeatures(lab_hor_denoised)
-features_data = np.column_stack((lab_ver_features, lab_hor_features))
-
-#Create TSFresh features for each projected dimension,
-#and stack both dimensions horizontally:
-lab_ver_for_tsf = TSFresh.convert_signals_for_ts_fresh(lab_ver_denoised,
-                                                       "ver")
-lab_ver_tsf_features = extract_features(lab_ver_for_tsf, column_id="signal_id",
-                                        column_sort="time")
-lab_hor_for_tsf = TSFresh.convert_signals_for_ts_fresh(lab_hor_denoised,
-                                                       "hor")
-lab_hor_tsf_features = extract_features(lab_hor_for_tsf, column_id="signal_id",
-                                        column_sort="time")
-features_data = pd.concat([lab_ver_tsf_features, lab_hor_tsf_features], axis=1)
-
-
-'''
-Prepare the data for the classification process:
-'''
-
+#Create features and labels data frames, according to the condition indicator:
 def create_labels(symptom_name, tags_data, condition_vector, binarize=True):
     if symptom_name == 'tremor':
         label_vector = tags_data.TremorGA.as_matrix()
@@ -133,10 +95,8 @@ def create_labels(symptom_name, tags_data, condition_vector, binarize=True):
         label_vector[label_vector>0] = 1
     return label_vector
 
-labels = create_labels('bradykinesia', tags_data=tags_df, condition_vector=cond,
-                       binarize=True)
-#features = features_data[cond==True]
-features = features_data.copy()
+labels = create_labels('bradykinesia', tags_data=tags_df, condition_vector=cond, binarize=True)
+features = features_data[cond==True]
 #tags_df_after_cond = tags_df[cond==True]
 patients = tags_df.SubjectId[cond==True]
 task_ids = tags_df.TaskID[cond==True]
@@ -144,8 +104,8 @@ task_ids = tags_df.TaskID[cond==True]
 '''
 Optimize the hyper-parameters of the classification model, using a leave-one-patient-out approach:
 '''
-optimized_model = classifier.optimize_hyper_params(features, labels, patients, 'xgboost',
-                                        hyper_params=None, scoring_measure = None, eval_iterations = 30)
+optimized_model = classifier.optimize_hyper_params(features, labels, patients, 'random_forest',
+                                        hyper_params=None, scoring_measure = None,eval_iterations = 30)
 
 '''
 Make predictions for each segment in the data.
@@ -174,7 +134,7 @@ agg_features = agg_segments_df[[x for x in agg_segments_df.columns if x not in [
 opt_model_for_agg_segments = classifier.optimize_hyper_params(agg_features, agg_labels, agg_patients,
                                                    model_name='random_forest_for_agg',
                                                    hyper_params=None, scoring_measure=None,eval_iterations = 30)
-final_pred = classifier.make_cv_predictions_for_agg_segments(agg_segments_df, opt_model_for_agg_segments, binary_class_thresh=0.5)
+final_pred = classifier.make_cv_predictions_for_agg_segments(agg_segments_df, opt_model_for_agg_segments)
 
 
 '''
