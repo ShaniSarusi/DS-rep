1e6bf4e51c24d4ae72f067100bb6d37b17dd5df5
awagner <avishai.wagner@intel.com>
2017-06-22 08:09:08 +0300

    projection is pep8

--- Utils/Preprocessing/projections.py
+++ Utils/Preprocessing/projections.py
@@ -7,22 +7,18 @@ Created on Thu May 25 15:36:46 2017
 
 import numpy as np
 from future.utils import lmap
-import pywt
 
-#    import sys
-#    sys.path.insert(0, 'C:\\Users\\imazeh\\Itzik\\Health_prof\\git4\\DataScientists\\LDopa\\useful_packages\\')
-#    from FunctionForPredWithDEEP import projGrav
 
-"""
-Input:
-    x - x axis numpy array, every raw is sample
-    y - y axis numpy array, every raw is sample
-    z - z axis numpy array, every raw is sample
-Ouput:
-    ver_proj - vertical projection
-    hor_proj - horizantel projection
-"""
 def project_from_3_to_2_dims(x, y, z):
+    """
+    Input:
+        x - x axis numpy array, every raw is sample
+        y - y axis numpy array, every raw is sample
+        z - z axis numpy array, every raw is sample
+    Ouput:
+        ver_proj - vertical projection
+        hor_proj - horizantel projection
+    """
     XYZ = np.stack((x, y, z), axis=2)
     XYZ = np.reshape(XYZ, (np.shape(XYZ)[0], np.shape(XYZ)[1]*np.shape(XYZ)[2]))
     HR = lmap(projGrav, XYZ)
@@ -30,43 +26,47 @@ def project_from_3_to_2_dims(x, y, z):
     hor_proj = np.asarray([i[1] for i in HR])
     return ver_proj, hor_proj
 
-"""
-Input:
-    XYZ - a 2d array, each row is XYZ = np.stack((x, y, z), axis=2) 
-          where x, y , z are axis numpy array, every raw is sample
-Ouput:
-    ver_proj - vertical projection
-    hor_proj - horizantel projection
-"""
+
 def projGrav(XYZ):
-    XYZ = np.reshape(XYZ,(int(len(XYZ)/3),3))
-    ver = []; hor = []
-    G = [np.mean(XYZ[:,0]),np.mean(XYZ[:,1]),np.mean(XYZ[:,2])]
-    G_norm = G/np.sqrt(sum(np.power(G,2)))
-    for i in range(len(XYZ[:,0])):
-        ver.append(np.dot([XYZ[i,:]],G))
-        hor.append(np.sqrt(np.dot(XYZ[i,:]-ver[i]*G_norm,XYZ[i,:]-ver[i]*G_norm)))
-    ver = np.reshape(np.asarray(ver),len(ver))
+    """
+    Input:
+        XYZ - a 2d array, each row is XYZ = np.stack((x, y, z), axis=2)
+        where x, y , z are axis numpy array, every raw is sample
+    Ouput:
+        ver_proj - vertical projection
+        hor_proj - horizantel projection
+    """
+    XYZ = np.reshape(XYZ, (int(len(XYZ)/3), 3))
+    ver = []
+    hor = []
+    G = [np.mean(XYZ[:, 0]), np.mean(XYZ[:, 1]), np.mean(XYZ[:, 2])]
+    G_norm = G/np.sqrt(sum(np.power(G, 2)))
+    for i in range(len(XYZ[:, 0])):
+        ver.append(np.dot([XYZ[i, :]], G))
+        hor.append(np.sqrt(np.dot(XYZ[i, :]-ver[i]*G_norm, XYZ[i, :]-ver[i]*G_norm)))
+    ver = np.reshape(np.asarray(ver), len(ver))
     return np.asarray(ver), np.asarray(hor)
 
-"""
-Input:
-    x - time signal
-    y - time singal
-    z - time signal
-Output:
-    ver - vertical axis
-    hor - horizantal axis
-"""
-def projGrav_one_samp(x,y,z):
+
+def projGrav_one_samp(x, y, z):
+    """
+    Input:
+        x - time signal
+        y - time singal
+        z - time signal
+    Output:
+        ver - vertical axis
+        hor - horizantal axis
+    """
     XYZ = np.stack((x, y, z), axis=1)
-    ver = []; hor = []
-    G = [np.mean(XYZ[:,0]),np.mean(XYZ[:,1]),np.mean(XYZ[:,2])]
-    G_norm = G/np.sqrt(sum(np.power(G,2)))
-    for i in range(len(XYZ[:,0])):
-        ver.append(np.dot([XYZ[i,:]],G))
-        hor.append(np.sqrt(np.dot(XYZ[i,:]-ver[i]*G_norm,XYZ[i,:]-ver[i]*G_norm)))
-    ver = np.reshape(np.asarray(ver),len(ver))
+    ver = []
+    hor = []
+    G = [np.mean(XYZ[:, 0]), np.mean(XYZ[:, 1]), np.mean(XYZ[:, 2])]
+    G_norm = G/np.sqrt(sum(np.power(G, 2)))
+    for i in range(len(XYZ[:, 0])):
+        ver.append(np.dot([XYZ[i, :]], G))
+        hor.append(np.sqrt(np.dot(XYZ[i, :]-ver[i]*G_norm, XYZ[i, :]-ver[i]*G_norm)))
+    ver = np.reshape(np.asarray(ver), len(ver))
     return np.asarray(ver), np.asarray(hor)
 
 
@@ -86,56 +86,30 @@ def project_gravity(x, y, z, num_samples_per_interval=None, round_up_or_down='do
 
     # perform sliding windows
     idx_start = 0
-    v = []; h = []
+    v = []
+    h = []
     for i in range(n):  # TODO - chunk the samples more evenly by dividing len(x) each time
         idx_start = i * win_size
-        idx_end = (i +1) * win_size
+        idx_end = (i + 1) * win_size
         if i == n-1:  # last iteration
             idx_end = -1
-        x_i = x[idx_start:idx_end]; y_i = y[idx_start:idx_end]; z_i = z[idx_start:idx_end]
+        x_i = x[idx_start:idx_end]
+        y_i = y[idx_start:idx_end]
+        z_i = z[idx_start:idx_end]
         ver_i, hor_i = projGrav_one_samp(x_i, y_i, z_i)
-        v.append(ver_i); h.append(hor_i)
+        v.append(ver_i)
+        h.append(hor_i)
     return np.hstack(v), np.hstack(h)
 
 
-def projGrav_one_samp_try(x,y,z,num_of_interval = 1):
-    List_x = chunkIt(x, num_of_interval)
-    List_y = chunkIt(y, num_of_interval)
-    List_z = chunkIt(z, num_of_interval)
-    hor_per_interval = []; ver_per_interval = [];
-    for index_list in range(len(List_x)):       
-        print(index_list)
-        XYZ = np.stack((List_x[index_list], List_y[index_list], List_z[index_list]), axis=1)
-        ver = []; hor = []     
-        G = [np.mean(XYZ[:,0]),np.mean(XYZ[:,1]),np.mean(XYZ[:,2])]
-        G_norm = G/np.sqrt(sum(np.power(G,2)))
-        for i in range(len(XYZ[:,0])):
-            ver.append(np.dot([XYZ[i,:]],G))
-            hor.append(np.sqrt(np.dot(XYZ[i,:]-ver[i]*G_norm,XYZ[i,:]-ver[i]*G_norm)))
-        ver = np.reshape(np.asarray(ver),len(ver))
-        ver_per_interval.append(np.asarray(ver))
-        hor_per_interval.append(np.asarray(hor))
-    ver_output = np.hstack(ver_per_interval)
-    hor_output = np.hstack(hor_per_interval)
-    return ver_output, hor_output
-
-
 def chunkIt(seq, num):
-  avg = len(seq) / float(num)
-  print(avg)
-  out = []
-  last = 0.0
+    avg = len(seq) / float(num)
+    print(avg)
+    out = []
+    last = 0.0
 
-  while last < len(seq):
-    out.append(seq[int(last):int(last + avg)])
-    last += avg
+    while last < len(seq):
+        out.append(seq[int(last):int(last + avg)])
+        last += avg
 
-  return out
-
-
-
-
-
-
-
-
+    return out
