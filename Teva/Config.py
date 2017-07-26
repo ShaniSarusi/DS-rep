d686db6b8f2bef920e14ef82d6dd3611ee3f42b1
itzik-mazeh-intel <itzik.mazeh@intel.com>
2017-07-26 17:12:20 +0300

    refactor Teva analysis + add phone acc data

--- Teva/Config.py
+++ Teva/Config.py
@@ -13,28 +13,33 @@ exec_path = join('C:', sep, 'Users', 'imazeh', 'Itzik', 'Health_prof', 'git_team
 #os.chdir(join('C:', sep, 'Users', 'imazeh', 'Itzik', 'Health_prof', 'git_team', 'DataScientists', 'Teva'))
 data_path = join('C:', sep, 'Users', 'imazeh', 'Itzik', 'Health_prof', 'Datasets', 'Teva', 'data')
 
-clinic_assessments_watch_data_file_path = join(data_path, 'clinic_assessments_watch_acc_data.csv')
-clinic_assessments_watch_pkl_file_path = join(data_path, 'clinic_assessments_watch_acc_pkl.csv')
-
-clinic_steps_watch_data_file_path = join(data_path, 'clinic_steps_watch_acc_data.csv')
-clinic_steps_watch_pkl_file_path = join(data_path, 'clinic_steps_watch_acc_pkl.csv')
-
-clinic_steps_phone_data_file_path = join(data_path, 'clinic_steps_phone_acc_data.csv')
-clinic_steps_phone_pkl_file_path = join(data_path, 'clinic_steps_phone_acc_pkl.csv')
-
-home_assessments_watch_data_file_path = join(data_path, 'home_assessments_watch_acc_data.csv')
-home_assessments_watch_pkl_file_path = join(data_path, 'home_assessments_watch_acc_pkl.csv')
-
-home_steps_watch_data_file_path = join(data_path, 'home_steps_watch_acc_data.csv')
-home_steps_watch_pkl_file_path = join(data_path, 'home_steps_watch_acc_pkl.csv')
-
-home_steps_phone_data_file_path = join(data_path, 'home_steps_phone_acc_data.csv')
-home_steps_phone_pkl_file_path = join(data_path, 'home_steps_phone_acc_pkl.csv')
-
-home_reminders_watch_data_file_path = join(data_path, 'home_reminders_watch_acc_data.csv')
-home_reminders_watch_pkl_file_path = join(data_path, 'home_reminders_watch_acc_pkl.csv')
-
-
 valid_users = [8, 25, 45, 51, 57, 63, 74, 82, 94, 108]
 report_values = range(5)
 accelerometer_baseline = 1000
+
+
+# Watch accelerometer data:
+
+clinic_assessments_watch_acc_file_path = join(data_path, 'clinic_assessments_watch_acc_data.csv')
+clinic_assessments_watch_acc_pkl_file_path = join(data_path, 'clinic_assessments_watch_acc_pkl.p')
+
+clinic_steps_watch_acc_file_path = join(data_path, 'clinic_steps_watch_acc_data.csv')
+clinic_steps_watch_acc_pkl_file_path = join(data_path, 'clinic_steps_watch_acc_pkl.p')
+
+home_assessments_watch_acc_file_path = join(data_path, 'home_assessments_watch_acc_data.csv')
+home_assessments_watch_acc_pkl_file_path = join(data_path, 'home_assessments_watch_acc_pkl.p')
+
+home_steps_watch_acc_file_path = join(data_path, 'home_steps_watch_acc_data.csv')
+home_steps_watch_acc_pkl_file_path = join(data_path, 'home_steps_watch_acc_pkl.p')
+
+home_reminders_watch_acc_file_path = join(data_path, 'home_reminders_watch_acc_data.csv')
+home_reminders_watch_acc_pkl_file_path = join(data_path, 'home_reminders_watch_acc_pkl.p')
+
+
+# Phone accelerometer data:
+
+clinic_steps_phone_acc_file_path = join(data_path, 'clinic_steps_phone_acc_data.csv')
+clinic_steps_phone_acc_pkl_file_path = join(data_path, 'clinic_steps_phone_acc_pkl.p')
+
+home_steps_phone_acc_file_path = join(data_path, 'home_steps_phone_acc_data.csv')
+home_steps_phone_acc_pkl_file_path = join(data_path, 'home_steps_phone_acc_pkl.p')
--- Teva/DataIngestion.py
+++ Teva/DataIngestion.py
@@ -14,89 +14,134 @@ import Config as c
 valid_users = c.valid_users
 imp.reload(c)
 
-clinic_df = pd.read_csv(c.clinic_file_path)
-inds = pd.isnull(clinic_df).any(1).nonzero()[0]
-print('len of inds:', len(inds))
-print(clinic_df.user_id.unique())
-clinic_df = clinic_df[clinic_df.user_id.isin(valid_users)]
-# Convert relevant columns to datetime format:
-clinic_df['assessment_start'] = pd.to_datetime(clinic_df['assessment_start'])
-clinic_df['assessment_end'] = pd.to_datetime(clinic_df['assessment_end'])
-clinic_df['timestamp'] = pd.to_datetime(clinic_df['timestamp'])
+''' Read and ingest watch accelerometer data:'''
 
-sub_clinic_df = clinic_df[['user_id', 'assessment_id', 'assessment_start', 'assessment_end',
-                           'timestamp', 'x', 'y', 'z', 'patient_report_value', 'clinician_report_value']]
-sub_clinic_df.to_pickle(join(c.data_path, 'clinic_processed_pickle.p'))
-# sub_clinic_df = pd.read_pickle(join(c.data_path, 'clinic_processed_pickle.p'))
+#######################################################################
+clinic_assessments_watch_df = pd.read_csv(c.clinic_assessments_watch_acc_file_path)
+inds = pd.isnull(clinic_assessments_watch_df).any(1).nonzero()[0]
+print('len of inds:', len(inds))
+print(clinic_assessments_watch_df.user_id.unique())
+clinic_assessments_watch_df = clinic_assessments_watch_df[clinic_assessments_watch_df.user_id.isin(valid_users)]
+# Convert relevant columns to datetime format:
+clinic_assessments_watch_df['assessment_start'] = pd.to_datetime(clinic_assessments_watch_df['assessment_start'])
+clinic_assessments_watch_df['assessment_end'] = pd.to_datetime(clinic_assessments_watch_df['assessment_end'])
+clinic_assessments_watch_df['timestamp'] = pd.to_datetime(clinic_assessments_watch_df['timestamp'])
+
+sub_clinic_assessments_watch_df = clinic_assessments_watch_df[['user_id', 'assessment_id', 'assessment_start',
+                                                               'assessment_end', 'timestamp', 'x', 'y', 'z',
+                                                               'patient_report_value', 'clinician_report_value']]
+sub_clinic_assessments_watch_df.to_pickle(c.clinic_assessments_watch_acc_pkl_file_path)
 
 
 #######################################################################
-home_df = pd.read_csv(c.home_file_name)
-inds = pd.isnull(home_df).any(1).nonzero()[0]
+clinic_steps_watch_df = pd.read_csv(c.clinic_steps_watch_acc_file_path)
+inds = pd.isnull(clinic_steps_watch_df).any(1).nonzero()[0]
 print('len of inds:', len(inds))
-print(home_df.user_id.unique())
-home_df = home_df[home_df.user_id.isin(valid_users)]
+print(clinic_steps_watch_df.user_id.unique())
+clinic_steps_watch_df = clinic_steps_watch_df[clinic_steps_watch_df.user_id.isin(valid_users)]
+# Convert relevant columns to datetime format:
+clinic_steps_watch_df['assessment_start'] = pd.to_datetime(clinic_steps_watch_df['assessment_start'])
+clinic_steps_watch_df['assessment_end'] = pd.to_datetime(clinic_steps_watch_df['assessment_end'])
+clinic_steps_watch_df['timestamp'] = pd.to_datetime(clinic_steps_watch_df['timestamp'])
+
+sub_clinic_steps_watch_df = clinic_steps_watch_df[['user_id', 'assessment_id', 'assessment_start', 'assessment_end',
+                                                   'timestamp', 'step_name', 'x', 'y', 'z', 'patient_report_value',
+                                                   'clinician_report_value']]
+sub_clinic_steps_watch_df.to_pickle(c.clinic_steps_watch_acc_pkl_file_path)
+
+
+#######################################################################
+home_assessments_watch_df = pd.read_csv(c.home_assessments_watch_acc_file_path)
+inds = pd.isnull(home_assessments_watch_df).any(1).nonzero()[0]
+print('len of inds:', len(inds))
+print(home_assessments_watch_df.user_id.unique())
+home_assessments_watch_df = home_assessments_watch_df[home_assessments_watch_df.user_id.isin(valid_users)]
 # Rename for compatibility:
-home_df['patient_report_value'] = home_df['value']
+home_assessments_watch_df['patient_report_value'] = home_assessments_watch_df['value']
 # Convert relevant columns to datetime format:
-home_df['assessment_start'] = pd.to_datetime(home_df['assessment_start'])
-home_df['assessment_end'] = pd.to_datetime(home_df['assessment_end'])
-home_df['timestamp'] = pd.to_datetime(home_df['timestamp'])
+home_assessments_watch_df['assessment_start'] = pd.to_datetime(home_assessments_watch_df['assessment_start'])
+home_assessments_watch_df['assessment_end'] = pd.to_datetime(home_assessments_watch_df['assessment_end'])
+home_assessments_watch_df['timestamp'] = pd.to_datetime(home_assessments_watch_df['timestamp'])
 # Rearrange the columns and save:
-sub_home_df = home_df[['user_id', 'assessment_id', 'assessment_start', 'assessment_end',
-                       'timestamp', 'x', 'y', 'z', 'patient_report_value']]
-sub_home_df.to_pickle(join(c.data_path, 'home_processed_pickle.p'))
+sub_home_assessments_watch_df = home_assessments_watch_df[['user_id', 'assessment_id', 'assessment_start',
+                                                           'assessment_end', 'timestamp', 'x', 'y', 'z',
+                                                           'patient_report_value']]
+sub_home_assessments_watch_df.to_pickle(c.home_assessments_watch_acc_pkl_file_path)
 
 
 #######################################################################
-five_mins_df = pd.read_csv(c.five_mins_file_name)
-inds = pd.isnull(five_mins_df).any(1).nonzero()[0]
+home_steps_watch_df = pd.read_csv(c.home_steps_watch_acc_file_path)
+inds = pd.isnull(home_steps_watch_df).any(1).nonzero()[0]
 print('len of inds:', len(inds))
-print(five_mins_df.user_id.unique())
-five_mins_df = five_mins_df[five_mins_df.user_id.isin(valid_users)]
+print(home_steps_watch_df.user_id.unique())
+home_steps_watch_df = home_steps_watch_df[home_steps_watch_df.user_id.isin(valid_users)]
 # Rename for compatibility:
-five_mins_df['patient_report_value'] = five_mins_df['value']
-five_mins_df['reminder_id'] = five_mins_df['id']
+home_steps_watch_df['patient_report_value'] = home_steps_watch_df['value']
 # Convert relevant columns to datetime format:
-five_mins_df['reported_timestamp'] = pd.to_datetime(five_mins_df['reported_timestamp'])
-five_mins_df['reported_minus_5'] = pd.to_datetime(five_mins_df['reported_minus_5'])
-five_mins_df['timestamp'] = pd.to_datetime(five_mins_df['timestamp'])
+home_steps_watch_df['assessment_start'] = pd.to_datetime(home_steps_watch_df['assessment_start'])
+home_steps_watch_df['assessment_end'] = pd.to_datetime(home_steps_watch_df['assessment_end'])
+home_steps_watch_df['timestamp'] = pd.to_datetime(home_steps_watch_df['timestamp'])
 # Rearrange the columns and save:
-sub_five_mins_df = five_mins_df[['user_id', 'reminder_id', 'reported_timestamp', 'reported_minus_5',
-                                 'timestamp', 'x', 'y', 'z', 'patient_report_value']]
-sub_five_mins_df.to_pickle(join(c.data_path, 'reminder_five_minutes_processed_pickle.p'))
+sub_home_steps_watch_df = home_steps_watch_df[['user_id', 'assessment_id', 'assessment_start', 'assessment_end',
+                                               'timestamp', 'step_name', 'x', 'y', 'z', 'patient_report_value']]
+sub_home_steps_watch_df.to_pickle(c.home_steps_watch_acc_pkl_file_path)
 
 
 #######################################################################
-clinic_steps_df = pd.read_csv(c.clinic_w_steps_file_path)
-inds = pd.isnull(clinic_steps_df).any(1).nonzero()[0]
+home_reminders_watch_df = pd.read_csv(c.home_reminders_watch_acc_file_path)
+inds = pd.isnull(home_reminders_watch_df).any(1).nonzero()[0]
 print('len of inds:', len(inds))
-print(clinic_steps_df.user_id.unique())
-clinic_steps_df = clinic_steps_df[clinic_steps_df.user_id.isin(valid_users)]
-# Convert relevant columns to datetime format:
-clinic_steps_df['assessment_start'] = pd.to_datetime(clinic_steps_df['assessment_start'])
-clinic_steps_df['assessment_end'] = pd.to_datetime(clinic_steps_df['assessment_end'])
-clinic_steps_df['timestamp'] = pd.to_datetime(clinic_steps_df['timestamp'])
-
-sub_clinic_steps_df = clinic_steps_df[['user_id', 'assessment_id', 'assessment_start', 'assessment_end',
-                                       'timestamp', 'step_name', 'x', 'y', 'z', 'patient_report_value', 'clinician_report_value']]
-sub_clinic_steps_df.to_pickle(join(c.data_path, 'clinic_steps_processed_pickle.p'))
-
-
-#######################################################################
-home_steps_df = pd.read_csv(c.home_w_steps_file_name)
-inds = pd.isnull(home_steps_df).any(1).nonzero()[0]
-print('len of inds:', len(inds))
-print(home_steps_df.user_id.unique())
-home_steps_df = home_steps_df[home_steps_df.user_id.isin(valid_users)]
+print(home_reminders_watch_df.user_id.unique())
+home_reminders_watch_df = home_reminders_watch_df[home_reminders_watch_df.user_id.isin(valid_users)]
 # Rename for compatibility:
-home_steps_df['patient_report_value'] = home_steps_df['value']
+home_reminders_watch_df['patient_report_value'] = home_reminders_watch_df['value']
+home_reminders_watch_df['reminder_id'] = home_reminders_watch_df['id']
 # Convert relevant columns to datetime format:
-home_steps_df['assessment_start'] = pd.to_datetime(home_steps_df['assessment_start'])
-home_steps_df['assessment_end'] = pd.to_datetime(home_steps_df['assessment_end'])
-home_steps_df['timestamp'] = pd.to_datetime(home_steps_df['timestamp'])
+home_reminders_watch_df['reported_timestamp'] = pd.to_datetime(home_reminders_watch_df['reported_timestamp'])
+home_reminders_watch_df['reported_minus_5'] = pd.to_datetime(home_reminders_watch_df['reported_minus_5'])
+home_reminders_watch_df['timestamp'] = pd.to_datetime(home_reminders_watch_df['timestamp'])
 # Rearrange the columns and save:
-sub_home_steps_df = home_steps_df[['user_id', 'assessment_id', 'assessment_start', 'assessment_end',
-                                   'timestamp', 'step_name', 'x', 'y', 'z', 'patient_report_value']]
-sub_home_steps_df.to_pickle(join(c.data_path, 'home_steps_processed_pickle.p'))
+sub_home_reminders_watch_df = home_reminders_watch_df[['user_id', 'reminder_id', 'reported_timestamp',
+                                                       'reported_minus_5', 'timestamp', 'x', 'y', 'z',
+                                                       'patient_report_value']]
+sub_home_reminders_watch_df.to_pickle(c.home_reminders_watch_acc_pkl_file_path)
 
+
+''' Read and ingest watch accelerometer data:'''
+
+#######################################################################
+clinic_steps_phone_df = pd.read_csv(c.clinic_steps_phone_acc_file_path)
+inds = pd.isnull(clinic_steps_phone_df).any(1).nonzero()[0]
+print('len of inds:', len(inds))
+print(clinic_steps_phone_df.user_id.unique())
+clinic_steps_phone_df = clinic_steps_phone_df[clinic_steps_phone_df.user_id.isin(valid_users)]
+# Rename for compatibility:
+clinic_steps_phone_df['patient_report_value'] = clinic_steps_phone_df['patient_report_desc']
+clinic_steps_phone_df['clinician_report_value'] = clinic_steps_phone_df['clinician_report_desc']
+# Convert relevant columns to datetime format:
+clinic_steps_phone_df['assessment_start'] = pd.to_datetime(clinic_steps_phone_df['assessment_start'])
+clinic_steps_phone_df['assessment_end'] = pd.to_datetime(clinic_steps_phone_df['assessment_end'])
+clinic_steps_phone_df['timestamp'] = pd.to_datetime(clinic_steps_phone_df['timestamp'])
+
+sub_clinic_steps_phone_df = clinic_steps_phone_df[['user_id', 'assessment_id', 'assessment_start', 'assessment_end',
+                                                   'timestamp', 'step_name', 'x', 'y', 'z', 'patient_report_value',
+                                                   'clinician_report_value']]
+sub_clinic_steps_phone_df.to_pickle(c.clinic_steps_phone_acc_file_path)
+
+
+#######################################################################
+home_steps_phone_df = pd.read_csv(c.home_steps_phone_acc_file_path)
+inds = pd.isnull(home_steps_phone_df).any(1).nonzero()[0]
+print('len of inds:', len(inds))
+print(home_steps_phone_df.user_id.unique())
+home_steps_phone_df = home_steps_phone_df[home_steps_phone_df.user_id.isin(valid_users)]
+# Rename for compatibility:
+home_steps_phone_df['patient_report_value'] = home_steps_phone_df['patient_report_desc']
+# Convert relevant columns to datetime format:
+home_steps_phone_df['assessment_start'] = pd.to_datetime(home_steps_phone_df['assessment_start'])
+home_steps_phone_df['assessment_end'] = pd.to_datetime(home_steps_phone_df['assessment_end'])
+home_steps_phone_df['timestamp'] = pd.to_datetime(home_steps_phone_df['timestamp'])
+# Rearrange the columns and save:
+sub_home_steps_phone_df = home_steps_phone_df[['user_id', 'assessment_id', 'assessment_start', 'assessment_end',
+                                               'timestamp', 'step_name', 'x', 'y', 'z', 'patient_report_value']]
+sub_home_steps_phone_df.to_pickle(c.home_steps_phone_acc_pkl_file_path)
