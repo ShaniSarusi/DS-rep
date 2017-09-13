739060734e4e58a7967cf53643bfb051b10ef854
itzik-mazeh-intel <itzik.mazeh@intel.com>
2017-09-13 15:47:12 +0300

    Teva cont. analysis according to the new feature - norm's STD

--- Utils/Visualization/box_plot.py
+++ Utils/Visualization/box_plot.py
@@ -11,7 +11,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def create_box_plot(df, x_discrete_variable, y_cont_variable, all_possible_x_vals=None, plt_title=None):
+def create_box_plot(df, x_discrete_variable, y_cont_variable, with_outliers=True, all_possible_x_vals=None,
+                    plt_title=None):
     """
     Plot a box-plot of a continuous variable for each value of a discrete variable.
     
@@ -19,6 +20,7 @@ def create_box_plot(df, x_discrete_variable, y_cont_variable, all_possible_x_val
         df (Pandas DataFrame): Should contain both variables as columns.
         x_discrete_variable (str): The column's name in df.
         y_cont_variable (str): The column's name in df.
+        with_outliers (bool): Controls whether outliers will be displayed in the plot or not.
         all_possible_x_vals (list): A sorted list of discrete values for the x-axis in the plot.
                                     If None (default), will plot only values which are present in df.
                                     If all_possible_x_vals is provided, all values in this list will be plotted on the
@@ -33,7 +35,11 @@ def create_box_plot(df, x_discrete_variable, y_cont_variable, all_possible_x_val
     else:
         x_discrete_values = sorted(df[x_discrete_variable].unique().tolist())
     boxes_vals = [np.asarray(df[y_cont_variable][df[x_discrete_variable] == x]) for x in x_discrete_values]
-    plt.boxplot(boxes_vals)
+    if with_outliers:
+        sym = None
+    else:
+        sym = ''
+    plt.boxplot(boxes_vals, sym=sym)
     plt.xticks(range(1, len(x_discrete_values)+1), [str(int(x)) for x in x_discrete_values])
     plt.xlabel(x_discrete_variable)
     plt.ylabel(y_cont_variable)
