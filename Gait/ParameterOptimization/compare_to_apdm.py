96e404e45a8a63d702fb35894dd08b24be38de97
Zeev Waks <zeev.waks@intel.com>
2017-09-26 08:02:33 +0300

    improving compare to apdm plot appearance

--- Gait/ParameterOptimization/compare_to_apdm.py
+++ Gait/ParameterOptimization/compare_to_apdm.py
@@ -74,9 +74,10 @@ def compare_to_apdm(alg_data_file, algs, apdm_metrics, name_prefix="", show_plot
             ax_range = ax_max - ax_min
             axarr[a, b].set_xlim(ax_min, ax_max)
             axarr[a, b].set_ylim(ax_min, ax_max)
-            axarr[a, b].text(ax_min + 0.05 * ax_range, ax_min + 0.9 * ax_range, 'R= ' + str(round(corr_j, 3)))
+            axarr[a, b].text(ax_min + 0.05 * ax_range, ax_min + 0.8 * ax_range, 'R= ' + str(round(corr_j, 3)))
         #plt.tight_layout()
         plt.suptitle(name_prefix + metric + ' comparison')
+        plt.subplots_adjust(wspace=.4)
 
         # Save and show
         save_name = name_prefix + metric + '_comparison.png'
